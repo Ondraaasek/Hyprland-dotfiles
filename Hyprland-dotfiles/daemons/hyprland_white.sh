657c38ab4bd58526f298f21b5274b0cc53edebaf
Ondraaasek <167030976+Ondraaasek@users.noreply.github.com>
2024-07-01 22:30:45 +0200

    Add files via upload

---
+++ Hyprland-dotfiles/daemons/hyprland_white.sh
@@ -0,0 +1,4 @@
+cp ~/.config/hypr/desktops/hyprland.conf_mono_sharp ~/.config/hypr/hyprland.conf &
+cp ~/.config/waybar/desktops/style.css_white_top ~/.config/waybar/style.css &
+pkill waybar &
+swww img ~/wallpapers/monochrome_road.jpg
